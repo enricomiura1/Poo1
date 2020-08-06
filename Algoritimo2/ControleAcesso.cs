efe62ef5524d21458aa11e366c01a8adce6966f8
Enrico <enrico.epetro@gmail.com>
2020-08-05 21:32:02 -0300

    first commit

---
+++ Algoritimo2/ControleAcesso.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritimo2
+{
+    class ControleAcesso
+    {
+       public string login = "Enrico";
+        public string senha = "1010";
+
+        public bool EfetuarLogin()
+        {
+            bool retorno = false;
+            Console.WriteLine("Digite o login:");
+            string loginDigitado = Console.ReadLine();
+
+
+
+            if (loginDigitado == login)
+            {
+                int count = 0;
+
+                while (count < 3)
+                {
+                    Console.WriteLine("Digite a Senha:");
+                    string senhaDigitada = Console.ReadLine();
+
+                    if (senhaDigitada == senha)
+                    {
+                        retorno = true;
+                        break;
+                    }
+                    count++;
+                }
+               
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Algoritimo2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Algoritimo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ControleAcesso controleAcesso = new ControleAcesso();
+
+            bool logado = controleAcesso.EfetuarLogin();
+
+
+            if (logado)
+                Console.WriteLine("loguin efetuado com sucesso!");
+
+
+
+
+
+
+
+            ////Numeros primos
+            //Console.WriteLine("Digite um limite");
+            //int limite = Convert.ToInt32(Console.ReadLine());
+            //int[] indices = new int[4];
+            //indices[0] = 2;
+            //indices[1] = 3;
+            //indices[2] = 5;
+            //indices[3] = 7;
+
+
+
+
+
+                //for (int l = 0; l <= limite; l++)//////// varrendo todos os numeros ate o limite informado pelo usuario
+
+                //{
+                //    bool primo = true;
+
+                //    for (int i = 0; i < 4; i++)
+                //    {
+                //        int resto = l % indices[i];
+                //        if (l != indices[i] && resto == 0)
+                //        {
+                //            primo = false;
+                //            break;
+
+
+                //        }            
+                //    }
+
+                //    if (primo)
+                //        Console.WriteLine(l);
+
+                //}
+
+
+
+
+
+
+
+
+                /// Calculo imc
+                // Console.WriteLine("Digite seu peso (KG);");
+                // int peso = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine("Digite sua Altuta (m);");
+                // double altura = Convert.ToDouble(Console.ReadLine());
+
+                // double imc = peso / (altura * altura);
+                //string  resultado = "";
+
+                // if (imc < 17)
+                //     resultado = "Muito abaixo do peso";
+
+                // if (imc >= 17 && imc <= 18.49)
+                //     resultado = " Abaixo do peso";
+
+                // if (imc > 18.49 && imc < 25)
+                //     resultado = "Peso normal";
+
+                // if (imc > 25 && imc < 30)
+                //     resultado = "Acima do Peso ";
+
+                // if (imc >= 30 && imc < 35)
+                //     resultado = "Obesidade 1";
+
+                // if (imc >= 35 && imc < 40)
+                //     resultado = "Obesidade 2";
+
+                // if (imc >= 40) ;
+                //     resultado = "Tu é um elefante";
+
+                // Console.WriteLine(resultado);
+
+
+        }
+    }
+}
